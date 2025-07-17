6f08c987ea93b73129ea03a4f945d30a32d1e430
Axuaxi <rojiwa111@gmail.com>
2025-07-17 11:31:31 -0400

    Moved language map to its own file as well as editing the input/output language buttons

---
+++ src/main/java/Configuration/LanguageCodes.java
@@ -0,0 +1,43 @@
+package Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LanguageCodes {
+    public static final Map<String, String> LANGUAGES = new LinkedHashMap<>() {{
+        put("Arabic", "ar");
+        put("Bulgarian", "bg");
+        put("Czech", "cs");
+        put("Danish", "da");
+        put("German", "de");
+        put("Greek", "el");
+        put("English (Britain)", "en-gb");
+        put("English (American)", "en-us");
+        put("Spanish", "es");
+        put("Estonian", "et");
+        put("Finnish", "fi");
+        put("French", "fr");
+        put("Hungarian", "hu");
+        put("Indonesian", "id");
+        put("Italian", "it");
+        put("Japanese", "ja");
+        put("Korean", "ko");
+        put("Lithuanian", "lt");
+        put("Latvian", "lv");
+        put("Norwegian (Bokmål)", "nb");
+        put("Dutch", "nl");
+        put("Polish", "pl");
+        put("Portuguese (Brazilian)", "pt-br");
+        put("Portuguese (European)", "pt-pt");
+        put("Romanian", "ro");
+        put("Russian", "ru");
+        put("Slovak", "sk");
+        put("Slovenian", "sl");
+        put("Swedish", "sv");
+        put("Turkish", "tr");
+        put("Ukrainian", "uk");
+        put("Chinese", "zh");
+        put("Chinese (simplified)", "zh-hans");
+        put("Chinese (traditional)", "zh-hant");
+    }};
+}

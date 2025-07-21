fb4570d87fe9451bab292e3d9dd448d702c880c8
Mustafa Ali <mustafanajfi@gmail.com>
2025-07-21 06:08:47 -0400

    Implemented FontList file

--- src/main/java/Configuration/FontList.java
+++ src/main/java/Configuration/FontList.java
@@ -1,4 +1,17 @@
 package Configuration;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class FontList {
+    public static final Map<String, String> FONTS = new LinkedHashMap<>() {{
+        put("Arial", "Arial");
+        put("Verdana", "Verdana");
+        put("Tahoma", "Tahoma");
+        put("Trebuchet MS", "Trebuchet MS");
+        put("Times New Roman", "Times New Roman");
+        put("Georgia", "Georgia");
+        put("Garamond", "Garamond");
+        put("Courier New", "Courier New");
+        put("Brush Script MT", "Brush Script MT");
+    }};
 }

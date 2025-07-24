34717b2769963dad26232ad8b1741ccc2f062ac0
Axuaxi <rojiwa111@gmail.com>
2025-07-24 00:46:25 -0400

    Moved pom.xml out of idea so it could actually run maven, as well as finally implementing pdf importer support.

--- src/main/java/Book/BookImporterFactory.java
+++ src/main/java/Book/BookImporterFactory.java
@@ -8,9 +8,8 @@ public class BookImporterFactory {
         String name = file.getName().toLowerCase();
         if (name.endsWith(".txt")) {
             return new TxtBookImporter();
-            // TODO: UNCOMMENT AFTER PDF IS FIXED
-//        } else if (name.endsWith(".pdf")) {
-//            return new PdfBookImporter();
+        } else if (name.endsWith(".pdf")) {
+            return new PdfBookImporter();
         } else {
             throw new IllegalArgumentException("Unsupported file format: " + name);
         }
--- src/main/java/Book/PdfBookImporter.java
+++ src/main/java/Book/PdfBookImporter.java
@@ -1,18 +1,18 @@
 package Book;
-// TODO: MAKE THIS WORK IT DOESNT WORK RIGHT NOW CUS MAVEN'S DUMB ASF
-//
-//import org.apache.pdfbox.pdmodel.PDDocument;
-//import org.apache.pdfbox.text.PDFTextStripper;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//public class PdfBookImporter implements BookImporter {
-//
-//    @Override
-//    public String importBook(File file) throws IOException {
-//        try (PDDocument document = PDDocument.load(file)) {
-//            return new PDFTextStripper().getText(document);
-//        }
-//    }
-//}
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PdfBookImporter implements BookImporter {
+
+    @Override
+    public String importBook(File file) throws IOException {
+        try (PDDocument document = PDDocument.load(file)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(document);
+        }
+    }
+}

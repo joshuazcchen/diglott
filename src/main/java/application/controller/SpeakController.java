51e72c796031532153e9c87b6cb2676062134fe9
Nehan Punjani <nehan.punjani@gmail.com>
2025-08-04 12:30:15 -0400

    Implement dual-language TTS support in SpeechManager and controller logic

--- src/main/java/application/controller/SpeakController.java
+++ src/main/java/application/controller/SpeakController.java
@@ -1,17 +1,35 @@
 package application.controller;
 
+import application.interactor.SpeakWordsInteractor;
 import application.usecase.SpeakWordsUseCase;
 
 import java.util.List;
 
 public class SpeakController {
-    private final SpeakWordsUseCase speakUseCase;
 
-    public SpeakController(SpeakWordsUseCase speakUseCase) {
-        this.speakUseCase = speakUseCase;
+    private final SpeakWordsUseCase useCase;
+
+    public SpeakController(SpeakWordsUseCase useCase) {
+        this.useCase = useCase;
+    }
+
+    public void speak(String word) {
+        useCase.speak(word);
     }
 
     public void speakWords(List<String> words) {
-        speakUseCase.speakWords(words);
+        useCase.speakWords(words);
+    }
+
+    public void speakWordPair(String original, String translated) {
+        if (useCase instanceof SpeakWordsInteractor) {
+            ((SpeakWordsInteractor) useCase).speakWordPair(original, translated);
+        }
+    }
+
+    public void speakWord(String word, String languageCode) {
+        if (useCase instanceof SpeakWordsInteractor) {
+            ((SpeakWordsInteractor) useCase).speakWord(word, languageCode);
+        }
     }
 }
--- src/main/java/application/interactor/SpeakWordsInteractor.java
+++ src/main/java/application/interactor/SpeakWordsInteractor.java
@@ -2,10 +2,12 @@ package application.interactor;
 
 import application.usecase.SpeakWordsUseCase;
 import domain.gateway.Speaker;
+import infrastructure.tts.SpeechManager;
 
 import java.util.List;
 
 public class SpeakWordsInteractor implements SpeakWordsUseCase {
+
     private final Speaker speaker;
 
     public SpeakWordsInteractor(Speaker speaker) {
@@ -13,7 +15,24 @@ public class SpeakWordsInteractor implements SpeakWordsUseCase {
     }
 
     @Override
+    public void speak(String word) {
+        speaker.speak(word);
+    }
+
+    @Override
     public void speakWords(List<String> words) {
         speaker.speak(words);
+    }
+
+    public void speakWordPair(String original, String translated) {
+        if (speaker instanceof SpeechManager sm) {
+            sm.speakWordPair(original, translated);
+        }
+    }
+
+    public void speakWord(String word, String languageCode) {
+        if (speaker instanceof SpeechManager sm) {
+            sm.speakWord(word, languageCode);
+        }
     }
 }
